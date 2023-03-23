b97544fecb27d467e4bac6da9548857a3c519c85
Green-Ace <bezlepkin.s04@gmail.com>
2023-03-23 15:54:17 +0300

    added proj

---
+++ _build/third-party/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ace/Green-Ace/workspace/TimpLab5/third-party/gtest/googletest
+# Build directory: /home/ace/Green-Ace/workspace/TimpLab5/_build/third-party/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
