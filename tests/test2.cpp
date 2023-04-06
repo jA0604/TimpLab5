aad36809e9680369e0b935ed38712e6a916ab415
jA0604 <j1107ya@ya.ru>
2023-04-06 14:13:32 +0300

    Update test2.cpp

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -1,21 +1,33 @@
 #include <Account.h>
 #include <Transaction.h>
-#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-inline bool operator==(Account a, Account b) { return true; }
-
-class MockTransaction: public Transaction{
-  public:
-  MOCK_METHOD(void, SaveToDataBase, (Account& from, Account& to, int sum), (override));
-};
-
-//Тестирование класса Transaction
-
-TEST(Transaction, SaveToDataBase) {
-  MockTransaction tr;
-  Account from(1, 500);
-  Account to(2, 400);
-  EXPECT_CALL(tr, SaveToDataBase(from, to, 150)).Times(1);
-  tr.SaveToDataBase(from, to, 150);
+TEST(Transaction, Banking){
+//создаём константы. base_fee для полноценного теста должен быть хотя бы 51.
+	const int base_A = 5000, base_B = 5000, base_fee = 100;
+//создаём тестовые объекты
+	Account Alice(0,base_A), Bob(1,base_B);
+	Transaction test_tran;
+//проверяем конструктор по умолчанию и сеттеры-геттеры
+	ASSERT_EQ(test_tran.fee(), 1);
+	test_tran.set_fee(base_fee);
+	ASSERT_EQ(test_tran.fee(), base_fee);
+//проверяем случаи когда транзакция не проходит
+	ASSERT_THROW(test_tran.Make(Alice, Alice, 1000), std::logic_error);
+	ASSERT_THROW(test_tran.Make(Alice, Bob, -50), std::invalid_argument);
+	ASSERT_THROW(test_tran.Make(Alice, Bob, 50), std::logic_error);
+	if (test_tran.fee()*2-1 >= 100)
+		ASSERT_EQ(test_tran.Make(Alice, Bob, test_tran.fee()*2-1), false);
+//проверяем, что всё правильно лочится
+	Alice.Lock();
+	ASSERT_THROW(test_tran.Make(Alice, Bob, 1000), std::runtime_error);
+	Alice.Unlock();
+//проверяем что если входные параметры правильные, то транзакция проходит православно
+	ASSERT_EQ(test_tran.Make(Alice, Bob, 1000), true);
+	ASSERT_EQ(Bob.GetBalance(), base_B+1000);	
+	ASSERT_EQ(Alice.GetBalance(), base_A-1000-base_fee);
+//проверяем что транзакция не проходит, если не хватает средств
+	ASSERT_EQ(test_tran.Make(Alice, Bob, 3900), false);
+	ASSERT_EQ(Bob.GetBalance(), base_B+1000);	
+	ASSERT_EQ(Alice.GetBalance(), base_A-1000-base_fee);
 }
