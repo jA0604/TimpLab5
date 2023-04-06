f89c901067de0aecc3f2110c4f96bd9594079eae
jA0604 <j1107ya@ya.ru>
2023-04-06 14:11:44 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,39 +1,14 @@
-#include <Transaction.h>
 #include <Account.h>
-/*#include <gtest/gtest.h>*/
-#include <include/gmock/gmock.h>
+#include <gtest/gtest.h>
 
-inline bool operator==(Account a, Account b) { return true; }
-
-class MockAccount: public Account{
-  public:
-  MockAccount(int id, int balance):Account(id, balance){}
-  MOCK_METHOD(void, Unlock, (), (override));
-};
-
-//Тестирование класса Account
-
-TEST(Account, GetBalance){
-  MockAccount acc(1,500);
-  EXPECT_EQ(acc.Account::GetBalance(), 500);
-}
-
-TEST(Account, ChangeBalance) {
-  MockAccount acc(1, 500);
-  EXPECT_THROW(acc.Account::ChangeBalance(250), std::runtime_error);
-  acc.Account::Lock();
-  acc.Account::ChangeBalance(250);
-  EXPECT_EQ(acc.Account::GetBalance(), 750);
-}
-
-TEST(Account, Lock) {
-  MockAccount acc(1, 500);
-  acc.Account::Lock();
-  EXPECT_THROW(acc.Account::Lock(), std::runtime_error);
-}
-
-TEST(Account, Unlock) {
-  MockAccount acc(1, 500);
-  EXPECT_CALL(acc, Unlock()).Times(1);
-  acc.Unlock();
+TEST(Account, Banking){
+	Account test(0,0);
+	ASSERT_EQ(test.GetBalance(), 0);
+	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
+	test.Lock();
+	ASSERT_NO_THROW(test.ChangeBalance(100));
+	ASSERT_EQ(test.GetBalance(), 100);
+	ASSERT_THROW(test.Lock(), std::runtime_error);
+	test.Unlock();
+	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
 }
